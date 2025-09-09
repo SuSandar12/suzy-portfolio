15e4b7711946af279c0c823fb966360ca893923d
Hsu Sandar Swe <susandarswe.mm@gmail.com>
2025-09-09 23:42:25 +0800

    update images

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -8,7 +8,7 @@ const activities = [
     shortDesc: "Completed with 2nd place in Upper Myanmar region.",
     longDesc:
       "The Samsung Tech Institute: Mobile Application Training is an annual program designed to advance the knowledge and skills of university students in Myanmar in mobile and digital technology. Each year, selected students from computer studies universities undergo a two-month course covering Android mobile app development, theoretical lessons, and group practical sessions. In 2016, I joined the program and achieved 2nd place in the Upper Myanmar region, gaining hands-on experience in Android application development and innovation.",
-    images: ["/images/samsung1.jpg"],
+    images: ["/suzy-portfolio/images/samsung1.jpg"],
     ref:"https://news.samsung.com/global/creating-opportunities-with-computer-skills",
   },
   {
@@ -17,7 +17,7 @@ const activities = [
     shortDesc: "Represented Myanmar in an international ICT competition.",
     longDesc:
       "The InnoServe Awards is a prestigious annual international ICT competition with over 30 years of history. It brings together students worldwide to present innovative projects that bridge academic knowledge and industry needs.I participated as a representative of Myanmar, presenting an information management project alongside global peers. The competition attracted participants from 100+ universities and colleges worldwide, making it one of the largest and most significant ICT contests in Taiwan.",
-    images: ["/images/innoserve1.jpg","/images/innoserve2.jpg"],
+    images: ["/suzy-portfolio/images/innoserve1.jpg","/suzy-portfolio/images/innoserve2.jpg"],
     ref: "https://innoserve.tca.org.tw/en/about_contest.aspx",
   },
   {
@@ -26,7 +26,7 @@ const activities = [
     shortDesc: "Competed in regional programming contests in Yangon.",
     longDesc:
       "The ACM International Collegiate Programming Contest (ICPC) is one of the world’s most prestigious programming competitions. I took part in the Asia-Yangon Regional Contest, competing with talented students and programmers across the region to solve complex algorithmic and coding challenges under time pressure. This experience strengthened my problem-solving skills, teamwork, and ability to perform under competitive conditions.",
-    images: ["/images/icpc1.jpg"],
+    images: ["/suzy-portfolio/images/icpc1.jpg"],
     ref: "https://icpc.global/",
   },
   
@@ -36,14 +36,14 @@ const activities = [
     shortDesc: "Competed at Technological Universities and Computer University.",
     longDesc:
       "In 2017, I participated in an English Language Competition where I tested my communication and presentation skills.",
-    images: ["/images/english1.jpg"],
+    images: ["/suzy-portfolio/images/english1.jpg"],
   },{
     id: 5,
     title: "Ooredoo-UNDP Myanmar Hackathon 2019",
     shortDesc: "Awarded by the Women’s Empowerment sector as part of Team Divas.",
     longDesc:
       "At the Ooredoo-UNDP Hackathon 2019 in Yangon, our team Divas was awarded the Women’s Empowerment prize. We developed HearU, an innovative platform that enables employees to confidentially report workplace misconduct directly to HR or management.This solution was showcased during Demo Day at Ooredoo Myanmar Headquarters, with an audience of the startup community and potential investors.",
-    images: ["/images/hackathon1.jpg", "/images/hackathon2.jpg"],
+    images: ["/suzy-portfolio/images/hackathon1.jpg", "/suzy-portfolio/images/hackathon2.jpg"],
     ref: "https://www.qatar-tribune.com/article/169033/NATION/Ooredoo-UNDP-Myanmar-Hackathon-winners-showcase-their-innovations",
   },
 ];
